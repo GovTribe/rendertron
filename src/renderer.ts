5df11350e5f0482842f6a13a93ae2a2ecae90073
Jay Hariani <jhariani@gmail.com>
2019-09-04 13:46:11 -0400

    Use networkidle2

--- src/renderer.ts
+++ src/renderer.ts
@@ -129,7 +129,7 @@ export class Renderer {
     try {
       // Navigate to page. Wait until there are no oustanding network requests.
       response = await page.goto(
-          requestUrl, {timeout: this.config.timeout, waitUntil: 'networkidle0'});
+          requestUrl, {timeout: this.config.timeout, waitUntil: 'networkidle2'});
     } catch (e) {
       console.error(e);
     }
@@ -206,7 +206,7 @@ export class Renderer {
     try {
       // Navigate to page. Wait until there are no oustanding network requests.
       response =
-          await page.goto(url, {timeout: 10000, waitUntil: 'networkidle0'});
+          await page.goto(url, {timeout: 10000, waitUntil: 'networkidle2'});
     } catch (e) {
       console.error(e);
     }
